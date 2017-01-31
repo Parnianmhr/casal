124e0c89f4d07a93ba33c5a6843f1468f566a7ff
Haico27 <haico.wensink@gmail.com>
2017-01-31 14:40:18 +0100

    Create bookingform on bookingindexpage

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,19 @@
 class BookingsController < ApplicationController
+
+  def index
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new
+
+    redirect_to bookings_path, notice: "Thank you for your booking!"
+  end
+
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:starts_at, :ends_at)
+  end
 end
