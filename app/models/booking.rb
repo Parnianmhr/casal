990c153a8a3e9be265e67154efde3e675d796cd2
Haico27 <haico.wensink@gmail.com>
2017-02-01 14:26:16 +0100

    updated bookingmodel, emptied guestmodel

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,17 @@
 class Booking < ApplicationRecord
-  belongs_to :guest
   belongs_to :villa
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :date_of_birth, presence: true
+  validates :country, presence: true
+  validates :street_name, presence: true
+  validates :house_number, presence: true
+  validates :zipcode, presence: true
+  validates :city_of_residence, presence: true
+  validates :country_of_residence, presence: true
+  validates :phone_number, presence: true
+  validates :email_address, presence: true
 
   def set_total_price
     self.price = villa.price
