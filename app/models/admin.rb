ead772ca568229d6eca2e08d4fa45f1480a05f44
Robert Jan Hogerbrugge <rj.hogerbrugge@hotmail.com>
2017-02-02 18:39:43 +0100

    seeds and adminview

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,4 +1,5 @@
 class Admin < ApplicationRecord
+  has_many :bookings
 
   has_secure_password
 
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,22 +1,22 @@
 class Booking < ApplicationRecord
 
-  # belongs_to :villa
+  belongs_to :admin
+
+  #
+  # validates :first_name, presence: true
+  # validates :last_name, presence: true
+  # validates :date_of_birth, presence: true
+  # validates :street_name, presence: true
+  # validates :house_number, presence: true
+  # validates :zipcode, presence: true
+  # validates :city_of_residence, presence: true
+  # validates :country_of_residence, presence: true
+  # validates :phone_number, presence: true
+  # validates :email_address, presence: true
 
 
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :date_of_birth, presence: true
-  validates :street_name, presence: true
-  validates :house_number, presence: true
-  validates :zipcode, presence: true
-  validates :city_of_residence, presence: true
-  validates :country_of_residence, presence: true
-  validates :phone_number, presence: true
-  validates :email_address, presence: true
 
-
-
-  before_create :set_check_in_times
+  # before_create :set_check_in_times
 
   # def villa_available?
   #   villa.available? starts_at, ends_at
