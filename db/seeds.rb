174a70bf69668de3d9cf0a564a3e2cd4fe575147
KellyHuliselan <kellyhuliselan@gmail.com>
2017-02-03 15:36:55 +0100

    made logged in when login succeeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,8 +3,8 @@ Booking.delete_all
 Season.delete_all
 
 #Admins
-admin1 = Admin.create(email: "admin1@casalverde.eu", password: "abcd1234")
-admin2 = Admin.create(email: "admin2@casalverde.eu", password: "efgh5678")
+admin1 = Admin.create(name: "admin1", email: "admin1@casalverde.eu", password: "abcd1234")
+admin2 = Admin.create(name: "admin2", email: "admin2@casalverde.eu", password: "efgh5678")
 
 #Bookings
 booking1 = Booking.create!(starts_at: "2017-01-01", ends_at: "2017-01-10", price: 2499.00, total: 2499.00, prefix: "Mr.", first_name: "William", last_name: "Windsor", number_of_guests: 8, date_of_birth: "1982-01-08", country_of_residence: "United Kingdom", street_name: "Kensington Gardens", house_number: 1,
