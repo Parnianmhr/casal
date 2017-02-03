b22f02174f0c63c7c2278b4e11e6aba9e41e9c10
KellyHuliselan <kellyhuliselan@gmail.com>
2017-02-03 14:31:50 +0100

    removed villa model and table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170203124533) do
+ActiveRecord::Schema.define(version: 20170203133024) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,11 +80,4 @@ ActiveRecord::Schema.define(version: 20170203124533) do
     t.decimal  "service"
   end
 
-  create_table "villas", force: :cascade do |t|
-    t.decimal  "price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_foreign_key "bookings", "villas"
 end
